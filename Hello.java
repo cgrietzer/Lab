e94ea8b5213118b0640dce4303afb2ce044294c6
cgrietzer <46756632+cgrietzer@users.noreply.github.com>
2019-01-16 12:03:22 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Colden");
+		System.out.println("Hello Java");
+		System.out.println("Creating first program");
+		System.out.println("16/01/2019");
+		System.out.println("CMSC 255 Sec 003");
+		System.out.println();
+	}
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
